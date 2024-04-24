cbefcef811cb4582668d74cc5b896fe145ca9fb5
rickslinger06 <65360608+rickslinger06@users.noreply.github.com>
2024-04-23 22:49:03 -0400

    Update UserController.java

--- Backend/src/main/java/com/cheesebank/controllers/UserController.java
+++ Backend/src/main/java/com/cheesebank/controllers/UserController.java
@@ -7,6 +7,7 @@ import com.cheesebank.models.User;
 import com.cheesebank.services.EmailService;
 import com.cheesebank.services.TransactionHistoryService;
 import com.cheesebank.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,7 @@ public class UserController {
     private TransactionHistoryService ths;
     private EmailService emailService;
 
+    @Autowired
     public UserController(UserService userService, TransactionHistoryService ths, EmailService emailService) {
         this.userService = userService;
         this.ths = ths;
@@ -28,50 +30,34 @@ public class UserController {
     }
 
     @PostMapping("/transaction")
-    public ResponseEntity<TransactionHistory> deposit
-            (@RequestParam int userId,
-             @RequestParam double amount,
-             @RequestParam String email,
-             @RequestParam String description,
-             @RequestParam TransactionType type){
+    public ResponseEntity<TransactionHistory> deposit(
+            @RequestParam int userId,
+            @RequestParam double amount,
+            @RequestParam String email,
+            @RequestParam String description,
+            @RequestParam String type) {
 
         User user = userService.findByUserId(userId);
+        double currentBalance = user.getBalance();
 
-        double newBalance = user.getBalance();
-
-       if(type.equals(TransactionType.DEPOSIT)){
-            newBalance = user.getBalance() + amount;
-           user.setBalance(newBalance);
-           emailService.sendEmail(email,"Deposit Notification","Dear Customer " +"\n \n"+"$"+amount+ " has been deposited to your account"+"\n \nCheese Bank");
-       }
-
-        if(type.equals(TransactionType.WITHDRAWAL)){
-             newBalance = user.getBalance() - amount;
-            if(newBalance < 0){
-                emailService.sendEmail(email,"Insufficient funds Notification","Dear Customer " +"\n \n"+ "You cannot withdraw from your account because you have you have insufficient "+"\n \nCheese Bank");
-                throw new TransactionCannotBeProcessException("You cannot withdraw the " +amount+" due to insufficient funds");
-
+        if ("DEPOSIT".equals(type)) {
+            user.setBalance(currentBalance + amount);
+            emailService.sendEmail(email, "Deposit Notification", "Dear Customer,\n\n$" + amount + " has been deposited to your account.\n\nCheese Bank");
+        } else if ("WITHDRAWAL".equals(type)) {
+            if (currentBalance < amount) {
+                emailService.sendEmail(email, "Insufficient Funds Notification", "Dear Customer,\n\nYou cannot withdraw from your account due to insufficient funds.\n\nCheese Bank");
+                throw new TransactionCannotBeProcessException("You cannot withdraw $" + amount + " due to insufficient funds");
             }
-            user.setBalance(newBalance);
-            emailService.sendEmail(email,"Withdrawal Notification","Dear Customer " +"\n \n"+"$"+amount+ " has been withdrawn from your account"+"\n \nCheese Bank");
-
+            user.setBalance(currentBalance - amount);
+            emailService.sendEmail(email, "Withdrawal Notification", "Dear Customer,\n\n$" + amount + " has been withdrawn from your account.\n\nCheese Bank");
+        } else {
+            throw new IllegalArgumentException("Invalid transaction type: " + type);
         }
 
         userService.updateUser(user);
 
-        TransactionHistory newTransaction = new TransactionHistory();
-        newTransaction.setType(type);
-        newTransaction.setDescription(description);
-        newTransaction.setTransaction_amount(amount);
-        newTransaction.setTimeStamp(LocalDateTime.now());
-        newTransaction.setCurrent_balance(newBalance);
-        newTransaction.setUser(user);
+        TransactionHistory th = ths.logTransaction(type,description,amount, currentBalance,user);
 
-        ths.logTransaction(newTransaction);
-
-        return ResponseEntity.status(HttpStatus.OK).body(newTransaction);
-
-        //test
-
+        return ResponseEntity.status(HttpStatus.OK).body(th);
     }
 }
