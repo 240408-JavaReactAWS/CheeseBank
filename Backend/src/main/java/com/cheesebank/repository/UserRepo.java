8f9bd0be7c8d925dfe0f25071df262b75018ab53
slhitney <slhitney@uncg.edu>
2024-04-24 15:54:24 -0400

    sav's user stories

--- Backend/src/main/java/com/cheesebank/repository/UserRepo.java
+++ Backend/src/main/java/com/cheesebank/repository/UserRepo.java
@@ -2,9 +2,16 @@ package com.cheesebank.repository;
 
 import com.cheesebank.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
     Optional<User> findUserByUsername(String username);
+    @Query("From User WHERE username = :usernameVar AND password = :passwordVar")
+    User findByUsernameAndPassword(@Param("usernameVar") String username, @Param("passwordVar") String password);
+
+    Optional<User> findUserByUsernameAndPassword(String username, String password);
+    //double check the find by user and password...
 }
--- Backend/src/main/java/com/cheesebank/services/UserService.java
+++ Backend/src/main/java/com/cheesebank/services/UserService.java
@@ -2,9 +2,12 @@ package com.cheesebank.services;
 
 import com.cheesebank.models.User;
 import com.cheesebank.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +17,7 @@ public class UserService {
 
     private UserRepo userRepo;
 
+    @Autowired
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
     }
@@ -34,5 +38,36 @@ public class UserService {
     }
     ////////////////////////////////////////////////////////
 
+    //Sav's code
+    public User findUserByUsernameAndPassword(String username, String password) {
+        Optional<User> optionalUser = userRepo.findUserByUsernameAndPassword(username, password);
 
+        if (optionalUser.isPresent()){
+            User myUser = optionalUser.get();
+            return myUser;
+        }
+        else{
+            return null;
+        }
+    }
+
+    //User story 1: As a user, I can register my account.
+    public User createUserAccount(User user){
+        return this.userRepo.save(user);
+    }
+
+    //User story 2: As a user, I can log in to my account.
+    public User processLogin(User user){
+        User myUser = findUserByUsernameAndPassword(user.getUsername(), user.getPassword());
+
+        if (myUser != null){
+            return myUser;
+        }
+        else{
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    //User story 3: As a user, I can update my personal information such as name,email, and phone number.
+    // note: does the above method "update user" do this?...
 }
