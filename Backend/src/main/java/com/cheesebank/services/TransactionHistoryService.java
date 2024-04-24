022c7a2825fa44506b6485a73432b2e0750dac00
rickslinger06 <65360608+rickslinger06@users.noreply.github.com>
2024-04-23 22:50:35 -0400

    Update TransactionHistoryService.java

--- Backend/src/main/java/com/cheesebank/services/TransactionHistoryService.java
+++ Backend/src/main/java/com/cheesebank/services/TransactionHistoryService.java
@@ -1,21 +1,34 @@
 package com.cheesebank.services;
 
 import com.cheesebank.models.TransactionHistory;
+import com.cheesebank.models.TransactionType;
+import com.cheesebank.models.User;
 import com.cheesebank.repository.TransactionHistoryRepo;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class TransactionHistoryService {
 
-    private TransactionHistoryRepo thr;
+    private final TransactionHistoryRepo thr;
 
     public TransactionHistoryService(TransactionHistoryRepo thr) {
         this.thr = thr;
     }
 
-    public void logTransaction(TransactionHistory newTransaction) {
 
+
+    public TransactionHistory logTransaction(String type, String description, double amount, double currentBalance, User user) {
+        TransactionHistory newTransaction = new TransactionHistory();
+        newTransaction.setType(TransactionType.valueOf(type));
+        newTransaction.setDescription(description);
+        newTransaction.setTransaction_amount(amount);
+        newTransaction.setTimeStamp(LocalDateTime.now());
+        newTransaction.setCurrent_balance(user.getBalance());
+        newTransaction.setUser(user);
         thr.save(newTransaction);
 
+        return newTransaction;
     }
 }
