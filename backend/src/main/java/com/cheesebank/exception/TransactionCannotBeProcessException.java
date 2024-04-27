910170e9accef5ecc2e99a1519437cf3de472452
Matthew Kwon <matthew.o.kwon@gmail.com>
2024-04-27 01:26:56 -0700

    Added exception handler for instances when withdrawals/transfers exceed account balance

--- backend/src/main/java/com/cheesebank/exception/TransactionCannotBeProcessException.java
+++
@@ -1,7 +0,0 @@
-package com.cheesebank.exception;
-
-public class TransactionCannotBeProcessException extends RuntimeException {
-    public TransactionCannotBeProcessException(String s) {
-        super(s);
-    }
-}
--- backend/src/main/java/com/cheesebank/exception/TransactionHistoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cheesebank.exception;
-
-public class TransactionHistoryNotFoundException extends RuntimeException {
-    public TransactionHistoryNotFoundException(String s) {
-        super(s);
-    }
-}
