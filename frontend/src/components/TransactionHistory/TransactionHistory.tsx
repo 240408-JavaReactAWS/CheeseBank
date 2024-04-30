e02263690a7c543256bf8f81209c28c5bbc161d9
Matthew Kwon <matthew.o.kwon@gmail.com>
2024-04-30 09:32:30 -0700

    Added view more/show less functionality to the transaction history

--- frontend/src/components/TransactionHistory/TransactionHistory.tsx
+++ frontend/src/components/TransactionHistory/TransactionHistory.tsx
@@ -1,100 +1,105 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Transaction } from '../../models/Transaction';
-import './TransactionHistory.css';
 import { useSession } from '../../context/SessionContext';
+import { Container, Table, Form, Button } from 'react-bootstrap';
 
 interface ResponseData {
     content: Transaction[];
 }
 
 function TransactionHistory() {
-    const { sessionUser } = useSession();
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-    const [searchText, setSearchText] = useState('');
-    const [filteredTransactions, setFilteredTransactions] = useState<Transaction[]>([]);
+  const { sessionUser } = useSession();
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [filteredTransactions, setFilteredTransactions] = useState<Transaction[]>([]);
+  const [showAll, setShowAll] = useState(false);
 
-    useEffect(() => {
-        const getAllTransactionHistory = async () => {
-            setLoading(true);
-            try {
-                const response = await axios.get<ResponseData>(`${process.env.REACT_APP_BACKEND_URL}/api/transactions/history`, {
-                    withCredentials: true
-                });
-                setTransactions(response.data.content);
-                setFilteredTransactions(response.data.content);
-            } catch (error) {
-                setError(true);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        if (sessionUser) {
-            getAllTransactionHistory();
+  useEffect(() => {
+      const getAllTransactionHistory = async () => {
+        setLoading(true);
+        try {
+          const response = await axios.get<ResponseData>(`${process.env.REACT_APP_BACKEND_URL}/api/transactions/history`, {
+              withCredentials: true
+          });
+          setTransactions(response.data.content);
+          setFilteredTransactions(response.data.content);
+        } catch (error) {
+          setError(true);
+        } finally {
+          setLoading(false);
         }
-    }, [sessionUser]);
+      };
 
-    const search = (searchText: string) => {
-        const filtered = transactions.filter(transaction => {
-            const description = transaction.description?.toLowerCase() ?? '';
-            const type = transaction.transactionType.toLowerCase();
-            return description.includes(searchText.toLowerCase()) || type.includes(searchText.toLowerCase());
-        });
-        setFilteredTransactions(filtered);
-    };
+      if (sessionUser) {
+        getAllTransactionHistory();
+      }
+  }, [sessionUser]);
 
-    const handleSearchInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const searchValue = e.target.value;
-        setSearchText(searchValue);
-        search(searchValue);
-    };
+  const search = (searchText: string) => {
+    const filtered = transactions.filter(transaction => {
+      const description = transaction.description?.toLowerCase() ?? '';
+      const type = transaction.transactionType.toLowerCase();
+      return description.includes(searchText.toLowerCase()) || type.includes(searchText.toLowerCase());
+    });
+    setFilteredTransactions(filtered);
+  };
 
-    if (!sessionUser) {
-        return <div>Please log in to view this page.</div>;
-    }
+  const handleSearchInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.target.value;
+    setSearchText(searchValue);
+    search(searchValue);
+  };
 
-    return (
-        <div className='transaction-history'>
-            <h2>Transaction History</h2>
-            <input
-                className='search-bar'
-                type="text"
-                value={searchText}
-                onChange={handleSearchInputChange}
-                placeholder="Search..."
-            />
+  if (!sessionUser) {
+    return <div>Please log in to view this page.</div>;
+  }
 
-            {loading && <p>Loading...</p>}
-            {error && <p>Error fetching data</p>}
-            <table>
-                <thead>
-                    <tr>
-                        <th>Transaction ID</th>
-                        <th>Transaction Type</th>
-                        <th>Transaction Amount</th>
-                        <th className='description'>Description</th>
-                        <th>Date</th>
-                        <th>Current Balance</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredTransactions.map((transaction) => (
-                        <tr key={transaction.id}>
-                            <td>{transaction.id}</td>
-                            <td>{transaction.transactionType}</td>
-                            <td>{transaction.transactionType === 'WITHDRAWAL' ? `(${transaction.amount})` : transaction.amount}</td>
-                            <td>{transaction.description}</td>
-                            <td>{new Date(transaction.timeStamp).toLocaleString()}</td>
-                            <td>{transaction.resultBalance}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  const transactionsToDisplay = showAll ? filteredTransactions : filteredTransactions.slice(0, 5);
+
+  return (
+    <Container>
+      <h2>Transaction History</h2>
+      <Form.Control
+        type="text"
+        value={searchText}
+        onChange={handleSearchInputChange}
+        placeholder="Search..."
+      />
+
+      {loading && <p>Loading...</p>}
+      {error && <p>Error fetching data</p>}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Transaction ID</th>
+            <th>Transaction Type</th>
+            <th>Transaction Amount</th>
+            <th>Description</th>
+            <th>Date</th>
+            <th>Current Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactionsToDisplay.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.id}</td>
+              <td>{transaction.transactionType}</td>
+              <td>{transaction.transactionType === 'WITHDRAWAL' ? `(${transaction.amount})` : transaction.amount}</td>
+              <td>{transaction.description}</td>
+              <td>{new Date(transaction.timeStamp).toLocaleString()}</td>
+              <td>{transaction.resultBalance}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Button onClick={() => setShowAll(!showAll)}>
+        {showAll ? 'Show Less' : 'View All Transactions'}
+      </Button>
+    </Container>
+  );
 }
 
 export default TransactionHistory;
