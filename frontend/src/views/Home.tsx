cd39b255a641becf1c797a0fecb958bcaf24fa4e
Matthew Kwon <matthew.o.kwon@gmail.com>
2024-04-30 07:27:19 -0700

    Replaced buttons with bootstrap Buttons on Home View

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from 'react';
 import Login from '../components/Login/Login';
 import Register from '../components/Register/Register';
 import ForgotPassword from '../components/ForgotPassword/ForgotPassword';
+import { Button } from 'react-bootstrap';
 
 const Home: React.FC = () => {
   const [view, setView] = useState<'login'| 'register' | 'forgot'>('login');
@@ -15,9 +16,9 @@ const Home: React.FC = () => {
       {view === 'login' && <Login />}
       {view === 'register' && <Register />}
       {view === 'forgot' && <ForgotPassword />}
-      {(view === 'login' || view === 'forgot') && <button aria-label="Register" onClick={() => handleViewChange('register')}>Sign up now</button>}
-      {(view === 'login' || view === 'register') && <button aria-label="Forgot Password" onClick={() => handleViewChange('forgot')}>Forgot password?</button>}
-      {view !== 'login' && <button aria-label="Back to Login" onClick={() => handleViewChange('login')}>Back to Login</button>}
+      {(view === 'login' || view === 'forgot') && <Button aria-label="Register" onClick={() => handleViewChange('register')}>Sign up now</Button>}
+      {(view === 'login' || view === 'register') && <Button aria-label="Forgot Password" onClick={() => handleViewChange('forgot')}>Forgot password?</Button>}
+      {view !== 'login' && <Button aria-label="Back to Login" onClick={() => handleViewChange('login')}>Back to Login</Button>}
     </div>
   );
 };
