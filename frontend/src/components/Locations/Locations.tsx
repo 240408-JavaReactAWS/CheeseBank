bb7e26e24cc32d777ab73051dd7aeb1ae64e7869
slhitney <slhitney@uncg.edu>
2024-04-30 13:15:41 -0400

    Navbar fixed, some other adjustments

---
+++ frontend/src/components/Locations/Locations.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Locations: React.FC = () => {
+    return (
+        <div>
+            <h1>Locations</h1>
+            <p>This feature is not yet supported.</p>
+        </div>
+    );
+};
+
+export default Locations;
