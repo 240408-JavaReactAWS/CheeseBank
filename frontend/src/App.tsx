87db65a8e1beddb00264a1105df12221c33c2c43
Matthew Kwon <matthew.o.kwon@gmail.com>
2024-04-29 20:46:49 -0700

    Cleaned up App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,24 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Dashboard from './views/Dashboard';
 import AdminDashboard from './views/AdminDashboard';
 import Home from './views/Home';
-import Transaction from './components/Transaction/Transaction';
-import Login from './components/Login/Login';
+import ResetPassword from './components/ResetPassword/ResetPassword';
 
-
-function App() {
+const App: React.FC = () => {
   return (
     <>
     <Header />
     <Navbar />
-
-    <Login/>
-    
-    <div className="content">
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/transaction" element={<Transaction />} />
       <Route path="/admin-dashboard" element={<AdminDashboard />} />
     </Routes>
-    </div>
-
     <Footer />
     </>
   );
